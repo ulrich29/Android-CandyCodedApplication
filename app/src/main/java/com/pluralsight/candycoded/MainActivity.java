2cf75488203d05d94a265021ba4d501e47ce44eb
bolyons <bolyons49@gmail.com>
2020-10-13 08:20:05 +0100

    Add_2

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -100,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Intent infoIntent = new Intent(this,InfoActivity.class);
+        startActivity(infoIntent);
         return super.onOptionsItemSelected(item);
     }
 }
